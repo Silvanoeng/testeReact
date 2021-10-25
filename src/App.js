c47e0fd444fd0257dfc770f29b586cf486625df8
Silvano AP <silvanoeng@hotmail.com>
2021-10-25 10:59:30 -0300

    teste page

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <h3>Silvano Araujo Pereira</h3>
+        <ul>
+          <li>Idade: 35 anos</li>
+          <li>Cidade: Mostardas/RS</li>
+          <li>GitHub: <a href= "https://github.com/Silvanoeng">Silvanoeng</a></li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
